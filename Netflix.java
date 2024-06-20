42f23973c8a4420237fc4ad9c2afb33fd413b979
bhumithv <bhumikadengimath@gmail.com>
2024-06-20 14:22:06 +0530

    My name is khan

---
+++ Netflix.java
@@ -0,0 +1,118 @@
+class Netflix{
+ public static void main(String n[]){
+  
+  String topTenMovies[]={"Hit Man", "Four Brothers", "Mission: Impossible-Dead Reckoning", "Mysteries of the Terracotta Warriors", "Safe House", "Ultraman: Rising", "How to Rob a Bank", "Atlas", "Home", "Wonder"};
+ 
+  String englishMovies[]={"Dracula", "Spiderman: No way home", "Safe House", "Extraction ", "Extraction 2", "Venom: Let there be change", "Jason Bourne", "Madame Web", "Damsel","Fast Ans Furious 7","Bad Boys For Life", "The Equalizer 3", "Exodus: Gods and kings", "Red Notice", "Heart Of Stones", "Rush Hour 3", "Interstellar", "Lift", "The Mummy", "Beast", "Army Of Thieves", "Luther: The fallen sun", "Men In Black: International", "The Conjuring 2", "Lords Of The Ring"};
+
+  String cartoonMovies[]={"The Angry Birds Movie", "The Angry Birds Movie 2", "Stuart Little", "Stuart Little 2", "Dora And The Lost City Of Gold", "The Monkey King", "Leo", "Ultraman: Rising", "Pokémon Detective Pikachu", "Peter Rabbit 2", "Madagascar", "The Casagrandes Movie", "Next Gen", "The Boss Baby", "DC League Of Supper-Pets"};
+ 
+  String hindiSeries[]={"Heeramandi: The Diamond Bazaar", "The Great Indian Kapil Show", "Amar Singh Chamkila", "Mission Majnu", "Jaane Jaan", "The Railway Men-The Untold Story Of Bhopal 1984", "Chor Nikal Ke Bhaga", "Ginny Weds Sunny", "Ghoul", "Sacred Games", "Delhi Crime", "Missmatched", "House Of Secrets", "Indian Matchmaking", "Feels Like Ishq"};
+  
+  String kSeries[]={"Crash Landing on You", "Extraordinary Attorney Woo", "Squid Game", "Kingdom", "Hellbound", "Our Beloved Summer", "Boys Over Flowers", "A Killer Paradox", "Parasyte: The Grey", "The Glory", "Business Proposal", "All of Us Are Dead", "D.P.", "Twenty-Five Twenty-One", "It's Okay to Not Be Okay", "Hometown Cha-Cha-Cha", "Mr.Sunshine", "Sweet Home", "A Time Called You","Little Women"};
+
+ System.out.println("Some movies and series recomendations to watch on Netflix:");
+ 
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+
+ System.out.println("Top ten movies on Netflix:"+""+topTenMovies[0]+"\n"
+ +""+topTenMovies[1]+"\n"
+ +""+topTenMovies[2]+"\n"
+ +""+topTenMovies[3]+"\n"
+ +""+topTenMovies[4]+"\n"
+ +""+topTenMovies[5]+"\n"
+ +""+topTenMovies[6]+"\n"
+ +""+topTenMovies[7]+"\n"
+ +""+topTenMovies[8]+"\n"
+ +""+topTenMovies[9]+" ");
+ 
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+  
+  System.out.println("25 English movies  available on  Netflix:"+" "+englishMovies[0]+"\n"
+ +""+englishMovies[1]+"\n"
+ +""+englishMovies[2]+"\n"
+ +""+englishMovies[3]+"\n"
+ +""+englishMovies[4]+"\n"
+ +""+englishMovies[5]+"\n"
+ +""+englishMovies[6]+"\n"
+ +""+englishMovies[7]+"\n"
+ +""+englishMovies[8]+"\n"
+ +""+englishMovies[9]+"\n"
+ +""+englishMovies[10]+"\n"
+ +""+englishMovies[11]+"\n"
+ +""+englishMovies[12]+"\n"
+ +""+englishMovies[13]+"\n"
+ +""+englishMovies[14]+"\n"
+ +""+englishMovies[15]+"\n"
+ +""+englishMovies[16]+"\n"
+ +""+englishMovies[17]+"\n"
+ +""+englishMovies[18]+"\n"
+ +""+englishMovies[19]+"\n"
+ +""+englishMovies[20]+"\n"
+ +""+englishMovies[21]+"\n"
+ +""+englishMovies[22]+"\n"
+ +""+englishMovies[23]+"\n"
+ +""+englishMovies[24]+" ");
+
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+
+ System.out.println("15 English cartoon movies  available on Netflix:"+" "+cartoonMovies[0]+"\n"
+ +""+cartoonMovies[1]+"\n"
+ +""+cartoonMovies[2]+"\n"
+ +""+cartoonMovies[3]+"\n"
+ +""+cartoonMovies[4]+"\n"
+ +""+cartoonMovies[5]+"\n"
+ +""+cartoonMovies[6]+"\n"
+ +""+cartoonMovies[7]+"\n"
+ +""+cartoonMovies[8]+"\n"
+ +""+cartoonMovies[9]+"\n"
+ +""+cartoonMovies[10]+"\n"
+ +""+cartoonMovies[11]+"\n"
+ +""+cartoonMovies[12]+"\n"
+ +""+cartoonMovies[13]+"\n"
+ +""+cartoonMovies[14]+" ");
+ 
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+
+ System.out.println("15 Hindi series available on Netflix:"+" "+hindiSeries[0]+"\n"
+ +""+hindiSeries[1]+"\n"
+ +""+hindiSeries[2]+"\n"
+ +""+hindiSeries[3]+"\n"
+ +""+hindiSeries[4]+"\n"
+ +""+hindiSeries[5]+"\n"
+ +""+hindiSeries[6]+"\n"
+ +""+hindiSeries[7]+"\n"
+ +""+hindiSeries[8]+"\n"
+ +""+hindiSeries[9]+"\n"
+ +""+hindiSeries[10]+"\n"
+ +""+hindiSeries[11]+"\n"
+ +""+hindiSeries[12]+"\n"
+ +""+hindiSeries[13]+"\n"
+ +""+hindiSeries[14]+" ");
+
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+
+ System.out.println("20 Korean series  available on  Netflix:"+" "+kSeries[0]+"\n"
+ +""+kSeries[1]+"\n"
+ +""+kSeries[2]+"\n"
+ +""+kSeries[3]+"\n"
+ +""+kSeries[4]+"\n"
+ +""+kSeries[5]+"\n"
+ +""+kSeries[6]+"\n"
+ +""+kSeries[7]+"\n"
+ +""+kSeries[8]+"\n"
+ +""+kSeries[9]+"\n"
+ +""+kSeries[10]+"\n"
+ +""+kSeries[11]+"\n"
+ +""+kSeries[12]+"\n"
+ +""+kSeries[13]+"\n"
+ +""+kSeries[14]+"\n"
+ +""+kSeries[15]+"\n"
+ +""+kSeries[16]+"\n"
+ +""+kSeries[17]+"\n"
+ +""+kSeries[18]+"\n"
+ +""+kSeries[19]+" ");
+
+ System.out.println("----------------------------------------------------------------------------------------------------------");
+}
+}
